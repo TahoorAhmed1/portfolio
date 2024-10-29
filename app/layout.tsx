3d1e1569053c0fd174396725794a5cb4317b25b8
Tahoor Ahmed <112215459+TahoorAhmed1@users.noreply.github.com>
2024-10-29 20:56:36 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,33 +9,25 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export const metadata = {
   title: "Tahoor Ahmed | Full Stack Developer",
   description: "Tahoor Ahmed Portfolio",
-  icons: [
-    {
-      rel: "icon",
-      url: "user.jpg",
-    },
-  ],
+  icons: [{ rel: "icon", url: "user.jpg" }],
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6230327304419894"
-          crossOrigin="anonymous"
-          strategy="beforeInteractive"
-        />
-        <Script async  strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-11280019357"></Script> <Script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11280019357'); </Script>
-<Script async  strategy="beforeInteractive" custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js" />head>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6230327304419894" crossOrigin="anonymous" strategy="beforeInteractive" />
+        <Script async strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-11280019357"></Script>
+        <Script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-11280019357');
+        </Script>
+      </head>
       <Provider>
         <body className={montserrat.className}>
-          <amp-analytics type="gtag" data-credentials="include"> <script type="application/json"> { "vars": { "gtag_id": "AW-11280019357", "config": { "AW-11280019357": { "groups": "default" } } }, "triggers": { } } </script> </amp-analytics>
+          <!-- Google tag (gtag.js) --> <amp-analytics type="gtag" data-credentials="include"> <script type="application/json"> { "vars": { "gtag_id": "AW-11280019357", "config": { "AW-11280019357": { "groups": "default" } } }, "triggers": { } } </script> </amp-analytics>
           <div className="dark:bg-[#0C0C1E]">
             <Header />
             <div>{children}</div>
