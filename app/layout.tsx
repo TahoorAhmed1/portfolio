323454a0d7662a7f41480a960d3b447f72e1f4de
Tahoor Ahmed <112215459+TahoorAhmed1@users.noreply.github.com>
2024-10-29 21:00:43 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Montserrat } from "next/font/google";
 import Provider from "@/components/Provider";
 import Header from "@/components/Header";
-import Script from 'next/script';
+import Script from "next/script";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,21 +16,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6230327304419894" crossOrigin="anonymous" strategy="beforeInteractive" />
-        <Script async strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-11280019357"></Script>
-        <Script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-11280019357');
+        <Script
+          async
+          strategy="beforeInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6230327304419894"
+          crossOrigin="anonymous"
+        />
+        <Script
+          async
+          strategy="beforeInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11280019357"
+        />
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11280019357');`}
         </Script>
       </head>
       <Provider>
         <body className={montserrat.className}>
-          <!-- Google tag (gtag.js) --> <amp-analytics type="gtag" data-credentials="include"> <script type="application/json"> { "vars": { "gtag_id": "AW-11280019357", "config": { "AW-11280019357": { "groups": "default" } } }, "triggers": { } } </script> </amp-analytics>
           <div className="dark:bg-[#0C0C1E]">
             <Header />
-            <div>{children}</div>
+            <main>{children}</main>
           </div>
         </body>
       </Provider>
