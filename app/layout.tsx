bd027c0d497f35e8fa10c1314fc77ff158644c14
Tahoor Ahmed <112215459+TahoorAhmed1@users.noreply.github.com>
2024-11-14 18:18:55 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,14 @@ import Provider from "@/components/Provider";
 import Header from "@/components/Header";
 import Script from "next/script";
 const montserrat = Montserrat({ subsets: ["latin"] });
+
 export const metadata = {
   title: "Tahoor Ahmed | Full Stack Developer",
   description: "Tahoor Ahmed Portfolio",
   icons: [{ rel: "icon", url: "user.jpg" }],
 };
-export default function RootLayout({ children }:any) {
+
+export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
       <head>
@@ -25,8 +27,9 @@ export default function RootLayout({ children }:any) {
           src="https://www.googletagmanager.com/gtag/js?id=AW-11280019357"
         />
         <Script
-            strategy="beforeInteractive"
-          id="google-analytics">
+          strategy="beforeInteractive"
+          id="google-analytics"
+        >
           {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -37,6 +40,20 @@ export default function RootLayout({ children }:any) {
         <body className={montserrat.className}>
           <div className="dark:bg-[#0C0C1E]">
             <Header />
+
+            {/* Google AdSense Ad Unit */}
+            <ins
+              className="adsbygoogle"
+              style={{ display: "block" }}
+              data-ad-client="ca-pub-6230327304419894"
+              data-ad-slot="1265784164"
+              data-ad-format="auto"
+              data-full-width-responsive="true"
+            ></ins>
+            <Script id="adsbygoogle-init" strategy="afterInteractive">
+              {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+            </Script>
+
             <main>{children}</main>
           </div>
         </body>
